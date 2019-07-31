9e6c915f78bbb58912366d6f0b21905b1ebb2658
Jordan Brandes <jordanbrandes@yahoo.com>
2019-07-31 10:46:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import io
 
+
 def contact():
         name = input("What is your contact's name? ")
         phone = int(input("What is your contacts number? "))
         email = input("What is your contact's email? ")
         print("Confirmed")
+        f.write("\n")
         f.write("Name: " + name)
         f.write("\n")
         f.write ("phone: " + str(phone))
         f.write("\n")
         f.write("email: " + email)
+        f.write("\n")
+        return json.dumps("contacts.json")
 
 f = open('contacts.txt', 'a')
 print("Welcome to your contact directory.")
