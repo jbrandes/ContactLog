a185e111ab3e56296cd5aed56591a26b7a0e3c11
Jordan Brandes <jordanbrandes@yahoo.com>
2019-07-31 10:26:39 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import io
+
+def contact():
+        name = input("What is your contact's name? ")
+        phone = int(input("What is your contacts number? "))
+        email = input("What is your contact's email? ")
+        print("Confirmed")
+        f.write("Name: " + name)
+        f.write("\n")
+        f.write ("phone: " + str(phone))
+        f.write("\n")
+        f.write("email: " + email)
+
+f = open('contacts.txt', 'w')
+print("Welcome to your contact directory.")
+print("Would you like to enter a new name into the directory? ")
+answer = input()
+if answer == "no":
+    print("Thanks for using our program.")
+if answer == "yes":
+    contact()
+    f.close()
