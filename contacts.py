440d01e8d24e43bc071a5ceeb9c0b6c1eb15efc1
Jordan Brandes <jordanbrandes@yahoo.com>
2019-08-06 16:29:48 -0500

    Update contacts.py

--- contacts.py
+++ contacts.py
@@ -1,9 +1,12 @@
 import io
 import json
-import requests
 import sqlite3
 
+conn = sqlite3.connect('contacts.sqlite')
 
+cursor = conn.cursor()
+
+print("Opened database successfully")
 
 def contact():
         name = input("What is your contact's name? ")
@@ -18,16 +21,20 @@ def contact():
         f.write("Email: " + email)
         f.write("\n")
         with open('contactlist.json', 'a') as outfile:
-            json.dump({'Name':name, 'Phone':phone, 'Email': email}, outfile)
-
-def sqldb():
-
-        conn = sqlite3.connect('test.db')
-        c = conn.cursor()
-        c.execute("CREATE TABLE IF NONE EXISTS contacts ( data json)")
-        
+            json.dump({'name':name, 'Phone':phone, 'Email': email}, outfile)
+            
+            cursor.execute('''CREATE TABLE CONTACTS
+         (ID INT PRIMARY KEY     NOT NULL,
+         NAME           TEXT    NOT NULL,
+         PHONE           INT     NOT NULL,
+         EMAIL        CHAR(50));''')
+            
+            
+conn.commit()
+conn.close()    
 
 f = open('contacts.txt', 'a')
+
 
     
 
